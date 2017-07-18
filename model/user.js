70642e772d110045b492a8ad63c6ebd23f97e75d
Romualdo <romualdoandre@gmail.com>
2017-07-17 23:36:30 -0300

    ficou complicado demais. voltar para o PHP

--- model/user.js
+++ model/user.js
@@ -1,4 +1,16 @@
+var sqlite3=require('sqlite3').verbose();
 
-module.exports.login=function(user,password){
-    return user=='jsday' && password=='jsday'
+function init(){
+    return new sqlite3.Database("../sorteio.sqlite");
+    
+}
+
+module.exports.login=function(login,password){
+    db=init();
+    var sucess=false;
+    db.get('select login from user where login=? and password=?',login,password,function(err,row){
+        sucess=row.login==login
+    })
+    db.close();
+    return sucess
 }
